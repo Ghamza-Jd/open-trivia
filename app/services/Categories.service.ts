b34f5bf5b32240538afac8ec9fa9f7ae0935b4f1
Hamza Jadid <hamzah.jadeed@gmail.com>
2021-12-25 10:51:59 +0200

    fix: fixed file naming convention

--- app/services/Categories.service.ts
+++ app/services/Categories.service.ts
@@ -1,4 +1,4 @@
-import {getCategories} from '&repos/web-repo/CategoryRepo';
+import {getCategories} from '&repos/web-repo/Category.repo';
 import {toTriviaCategory, TriviaCategory} from '&models/Category.model';
 
 export const getCategoriesService = async (): Promise<TriviaCategory[]> => {
